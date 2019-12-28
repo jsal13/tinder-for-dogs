527ee58996b3b0cbd34eb2b2cae99cd786eaa6b7
James Salvatore <jimmy.c.salvatore@gmail.com>
2019-12-28 03:23:14 -0600

    feat: added webapp to dockercompose;

--- api/api.py
+++ api/api.py
@@ -4,6 +4,7 @@ import json
 from flask import Flask
 from flask_restplus import Resource, Api
 from flask_restplus import reqparse
+from flask_cors import CORS
 
 import pymongo
 from bson import json_util
@@ -12,12 +13,13 @@ from bson import json_util
 # TODO (James): what's best practice for hosting the client?
 # How do I always open a new client on each request?
 
-MONGODB_LOCATION = os.environ.get("MONGODB_LOCATION", "mongodb://localhost:27017/")
+MONGODB_LOCATION = os.environ.get("MONGODB_LOCATION", "mongodb://mongodb:27017/")
 DB_CLIENT = pymongo.MongoClient(MONGODB_LOCATION)
 DB_DATABASE = DB_CLIENT["metadata"]  # metadata is the db name.
 
 # Flask app start.
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 # Get arguments for /dog endpoint.
@@ -30,10 +32,11 @@ dog_metadata_request_parser.add_argument(
 @api.route("/dog")
 @api.expect(dog_metadata_request_parser)
 class Dog(Resource):
-    def post(self):
+    def get(self):
         args = dog_metadata_request_parser.parse_args()
         dog_type = args["dog_type"]
-
+        print(args)
+        print("hello!")
         # ObjectId needs to be parsed, then we return it to json for the user.
         return json.loads(json_util.dumps(DB_DATABASE["dogs"].find({"Name": dog_type})))
 
@@ -42,7 +45,14 @@ class Dog(Resource):
 @api.route("/users")
 class User(Resource):
     def get(self):
-        return DB_DATABASE["users"].find()
+        return json.loads(json_util.dumps(DB_DATABASE["users"].find()))
+
+
+# List all users for some reason
+@api.route("/dogs")
+class User(Resource):
+    def get(self):
+        return json.loads(json_util.dumps(DB_DATABASE["dogs"].find()))
 
 
 if __name__ == "__main__":
