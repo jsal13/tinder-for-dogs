54a9f2fd24c83c65565655a916105aa21481e632
Crazyglue <edbz2k2@gmail.com>
2020-01-02 23:00:53 -0600

    feat(webapp): add vuetify and throw in a basic app layout

---
+++ webapp/src/components/SideNavContent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        VUE_APP_API_ADDRESS: <strong>{{ apiAddress }}</strong>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SideNavContent',
+    data: () => ({
+        apiAddress: process.env.VUE_APP_API_ADDRESS
+    })
+}
+</script>
